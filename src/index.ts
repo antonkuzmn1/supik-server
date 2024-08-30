5d42fbf6723bbd9ea97518e32ede6911612cccbe
anton <antonkuzmn1@gmail.com>
2024-08-30 08:57:16 +0700

    0.0.4-SNAPSHOT

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', router);
 
-const version = '0.0.3-SNAPSHOT';
+const version = '0.0.4-SNAPSHOT';
 
 const PORT: string | 3000 = process.env.PORT || 3000;
 
