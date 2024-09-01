1e077b9f943b63d895998fc2e791e72ee50a844e
anton <antonkuzmn1@gmail.com>
2024-09-01 09:23:33 +0700

    0.1.2-ALPHA

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', router);
 
-const version = '0.1.1-ALPHA';
+const version = '0.1.2-ALPHA';
 
 const PORT: string | 3000 = process.env.PORT || 3000;
 
