a21941b4150bb3aa75ad8e1c1caccd30749a878b
anton <antonkuzmn1@gmail.com>
2024-09-14 16:01:31 +0700

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', router);
 
-const version = '1.0.0';
+const version = '1.0.1';
 
 const PORT: string | 3000 = process.env.PORT || 3000;
 
