cf5c90842ed16d421fc5a791760c15c6387f316a
anton <antonkuzmn1@gmail.com>
2024-09-29 22:57:17 +0700

    24.10.7

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/api', router);
 
-const version = '24.10.0';
+const version = '24.10.7';
 
 const PORT: string | 3000 = process.env.PORT || 3000;
 
